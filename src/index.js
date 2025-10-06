c19d6872eb4c0d953d2033c55d2900c9ed465577
lddm4200 <longddm2k4@gmail.com>
2025-10-06 15:44:59 +0700

    size limit json

--- src/index.js
+++ src/index.js
@@ -5,21 +5,21 @@ import job from "./lib/cron.js";
 import authRoutes from "./routes/authRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cartRoutes from "./routes/cartRouter.js";
-
 import { connectDB } from "./lib/db.js";
 
 const app = express();
-// app.use(express.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 3000;
 
+// Bắt đầu cron job
 job.start();
-app.use(express.json());
+
+// Cấu hình middleware
 app.use(cors());
+app.use(express.json({ limit: '10mb' })); // Giới hạn kích thước JSON
+app.use(express.urlencoded({ limit: '10mb', extended: true })); // Giới hạn kích thước URL-encoded
 
-app.use(express.json({ limit: '20mb' }));
-app.use(express.urlencoded({ limit: '20mb', extended: true }));
-
+// Định nghĩa các route
 app.use("/api/auth", authRoutes);
 app.use("/api/product", productRoutes);
 app.use("/api/cart", cartRoutes);
@@ -29,6 +29,7 @@ app.use((req, res) => {
   res.status(404).send("404 Not Found: Trang không tồn tại.");
 });
 
+// Khởi động server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   connectDB();
